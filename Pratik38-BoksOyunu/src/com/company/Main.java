b9153996af94327df4686a1afe5485fede6b7495
Fazlı Keleş <61543926+fazlikeles@users.noreply.github.com>
2021-11-01 13:50:34 +0300

    Update Main.java
    Boksör isimleri kullanıcıdan alınır hale getirildi.

--- Pratik38-BoksOyunu/src/com/company/Main.java
+++ Pratik38-BoksOyunu/src/com/company/Main.java
@@ -11,12 +11,16 @@ public class Main {
         int health = random.nextInt(31)+70;
         int damage = random.nextInt(11);
         int sira = random.nextInt(2);
-        Fighter f1 = new Fighter("Muhammed Ali",damage,health,85,20);
+        String fighterName;
+        System.out.print("Birinci dövüşçünün ismini giriniz:");
+        fighterName = scan.nextLine();
+        Fighter f1 = new Fighter(fighterName,damage,health,85,20);
         // canlar ve güçler tekrar oluşturuldu.
         health = random.nextInt(31)+70;
         damage = random.nextInt(10);
-
-        Fighter f2 = new Fighter("Mike Tyson",damage,health,90,20);
+        System.out.print("Birinci dövüşçünün ismini giriniz:");
+        fighterName = scan.nextLine();
+        Fighter f2 = new Fighter(fighterName,damage,health,90,20);
         Match m1 = new Match(f1,f2,80,100,sira);
 
         m1.run();
